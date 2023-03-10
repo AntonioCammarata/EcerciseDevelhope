4eec2b9b82097b2722548bf3722e9e989fd927c4
AntonioCammarata <antoniocamm2001@gmail.com>
2023-03-10 01:07:31 +0100

    ExerciseDone

---
+++ Exercise7/exercise7.js
@@ -0,0 +1,19 @@
+
+function printName(){
+    let helloName = "Hello John";
+    
+    function inner(){
+        setTimeout(function(){
+            console.log(helloName)
+        },1000)
+        
+    }
+    return inner;
+}
+printName()();
+
+
+
+
+
+
