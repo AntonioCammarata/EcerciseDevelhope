066c955d7e1742f78c2ffe99cd97bf6ad653185c
AntonioCammarata <antoniocamm2001@gmail.com>
2023-03-10 10:32:02 +0100

    ExerciseDone

---
+++ Exercise9/exercise9.js
@@ -0,0 +1,11 @@
+const person = {
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+let result = Object.keys(person);
+let result_2 = Object.entries(person);
+
+console.log(result);
+console.log(result_2)
