9aa41b1b55147cc55b82dae686152ce5b281cb3c
AntonioCammarata <antoniocamm2001@gmail.com>
2023-03-22 17:20:10 +0100

    ExerciseDone

---
+++ Exercise25/exercise25.js
@@ -0,0 +1,5 @@
+let newNumber = [3];
+let numberStore = [0, 1, 2,...newNumber];
+
+
+console.log(numberStore);
---
+++ Exercise26/exercise26.js
@@ -0,0 +1,5 @@
+function sum(...args) {
+    return args;
+}
+
+console.log(sum(1, 2, 3, 4, 5));
